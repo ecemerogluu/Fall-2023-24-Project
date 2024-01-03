bbed0036bfa84d80567af222428135b5510a219d
ecemerogluu <154255717+ecemerogluu@users.noreply.github.com>
2024-01-03 23:34:42 +0300

    Update main.py

--- 343 Term Project/2023-24-Fall-Project/pythonfiles/main.py
+++ 343 Term Project/2023-24-Fall-Project/pythonfiles/main.py
@@ -1,8 +1,10 @@
 from csv_parser import CSVParser
 from knapsack import Knapsack
 from tsp import TSP
+import time
 
 def main():
+    start = time.time()
     songs = CSVParser.parse_songs("../data/songs_data.csv")
     file_name = "../data/cities.csv"
     cities = CSVParser.parse_cities(file_name)
@@ -10,7 +12,7 @@ def main():
     
     total_popularity = 0
     total_duration = 0
-    
+ 
     print("Tour route:")
     print("")
     for city in result[0]:
@@ -25,6 +27,7 @@ def main():
     print("Total distance:", result[1])
     print("Total popularity:", total_popularity)
     print("Total duration:", total_duration)
-
+    end = time.time()
+    print((end-start) * 10**3, "ms")
 if __name__ == "__main__":
     main()
