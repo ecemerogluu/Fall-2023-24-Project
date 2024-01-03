cf1a0dbd7bd7eb19e10654b25f0d95ac98fd3c2e
ecemerogluu <154255717+ecemerogluu@users.noreply.github.com>
2024-01-03 18:59:33 +0300

    Update knapsack.py

--- 343 Term Project/2023-24-Fall-Project/pythonfiles/knapsack.py
+++ 343 Term Project/2023-24-Fall-Project/pythonfiles/knapsack.py
@@ -1,8 +1,32 @@
+import numpy as np
+
 class Knapsack:
     @staticmethod
     def knapsack(songs, max_duration):
         num_songs = len(songs)
         selected_songs = []
-        # Knapsack algorithm implementation
-        # ... (use the previously provided knapsack function here)
-        return selected_songs  # Return selected songs based on knapsack algorithm
+        
+        durations = [song.duration for song in songs]
+        popularities = [song.popularity for song in songs]
+
+        table = np.zeros((num_songs+1, max_duration + 1))
+        
+        for i in range(0, num_songs):
+            for w in range(0, max_duration+1):
+                if durations[i] <= w:
+                    table[i+1, w] = max(table[i, w], popularities[i] + table[i, w - round(durations[i])])
+                else:
+                    table[i+1, w] = table[i, w]
+        
+        i = num_songs
+        w = max_duration
+        
+        while i > 0 and w > 0:
+            if table[i, w] == table[i-1, w]:
+                i -= 1
+            else:
+                w -= round(durations[i-1])
+                selected_songs.append(songs[i-1])
+                i -= 1
+        
+        return selected_songs
