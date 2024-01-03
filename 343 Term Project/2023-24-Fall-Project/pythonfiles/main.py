9c7e8f3842de84baa5c4a9f4310b925aca7c6c1a
ecemerogluu <154255717+ecemerogluu@users.noreply.github.com>
2024-01-03 19:00:08 +0300

    Update main.py

--- 343 Term Project/2023-24-Fall-Project/pythonfiles/main.py
+++ 343 Term Project/2023-24-Fall-Project/pythonfiles/main.py
@@ -3,16 +3,28 @@ from knapsack import Knapsack
 from tsp import TSP
 
 def main():
-    songs = CSVParser.parse_songs("songs_data.csv")
-    max_duration = 200  # Replace this value with your desired maximum duration
-    selected_songs = Knapsack.knapsack(songs, max_duration)
-    for song in selected_songs:
-        print(f"Song: Popularity - {song.popularity}, Duration - {song.duration}")
-
-    file_name = "data/cities.csv"
+    songs = CSVParser.parse_songs("../data/songs_data.csv")
+    file_name = "../data/cities.csv"
     cities = CSVParser.parse_cities(file_name)
     result = TSP.nearest_neighbor_tsp(cities)
-    # Other parts of your code
+    
+    total_popularity = 0
+    total_duration = 0
+    
+    print("Tour route:")
+    print("")
+    for city in result[0]:
+        print(city.get_name())
+        max_duration = city.get_duration()
+        selected_songs = Knapsack.knapsack(songs, max_duration)
+        for song in selected_songs:
+            total_popularity += song.popularity
+            total_duration += song.duration
+           
+    print("")
+    print("Total distance:", result[1])
+    print("Total popularity:", total_popularity)
+    print("Total duration:", total_duration)
 
 if __name__ == "__main__":
     main()
